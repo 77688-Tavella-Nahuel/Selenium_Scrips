6db511fea312c8c4ffefaadb708a72545b842298
Nahuel Tavella <nahuutavella@gmail.com>
2022-12-15 13:28:14 -0300

    Entrega final

--- TC_Form_Contact_Empty.py
+++ TC_Form_Contact_Empty.py
@@ -3,7 +3,9 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 import os
+from colorama import Fore, init, Back
 
+init(autoreset=True)
 os.system("cls")
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
@@ -11,37 +13,65 @@ options.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver=webdriver.Chrome(executable_path=r"C:\Drivers\chromedriver.exe", options=options)
 driver.maximize_window()
 
+contar_error = 0
+
 #Abrir sitio web
 driver.get("https://centroestant.com.ar/")
 title = driver.title
-assert "INICIO - CENTRO ESTANT" == title, "No coinciden las páginas"
+if "INICIO - CENTRO ESTANT" == title:
+    print("Ingreso a página principal: "+Fore.GREEN+"PASS") 
+else:
+    print("Ingreso a página principal: "+Fore.RED+"FAIL")
+    contar_error += 1
 
 #Abrir link "Contacto"
 click_button = driver.find_element(by=By.LINK_TEXT, value="Contacto")
 click_button.click()
 
 contacto = "Déjanos tu mensaje y te responderemos con la mayor brevedad posible."
-h3 = driver.find_element(by=By.XPATH,value="//*[@id='col-1546266161']/div/h3/span").text
-assert contacto == h3, "No se ingreso a la seccion 'Nosotros'"
+h3 = driver.find_element(by=By.XPATH,value="/html/body/div[1]/main/div/div/div/div/div/div/div/h3/span").text
+if contacto == h3:
+    print("Ingreso a la sección 'Contacto': "+Fore.GREEN+"PASS") 
+else:
+    print("Ingreso a la sección 'Contacto': "+Fore.RED+"FAIL")
+    contar_error += 1
 
 time.sleep(2)
 
 #Enviar formulario vacio
-click_button_send = driver.find_element(by=By.XPATH, value="//*[@id='wpcf7-f11-p648-o1']/form/p[6]/input")
+click_button_send = driver.find_element(by=By.XPATH, value="/html/body/div[1]/main/div/div/div/div/div/div/div/div[2]/form/p[6]/input")
 click_button_send.click()
+
 time.sleep(4)
 
 formulario_vacio = "Uno o más campos tienen un error. Por favor, revísalos e inténtalo de nuevo."
-texto = driver.find_element(by=By.XPATH,value="//*[@id='wpcf7-f11-p648-o1']/form/div[2]").text
-assert formulario_vacio == texto, "No se envio el formulario vacio"
+texto = driver.find_element(by=By.XPATH,value="/html/body/div[1]/main/div/div/div/div/div/div/div/div[2]/form/div[2]").text
+if formulario_vacio == texto:
+    print("Envío de formulario vacío: "+Fore.GREEN+"PASS") 
+else:
+    print("Envío de formulario vacío: "+Fore.RED+"FAIL")
+    contar_error += 1 
 
 time.sleep(2)
 
 #Volver a la home
-volver_home = driver.find_element(by=By.XPATH, value="//*[@id='logo']/a/img[1]")
+volver_home = driver.find_element(by=By.XPATH, value="/html/body/div[1]/header/div/div[2]/div[1]/div[1]/a/img[1]")
 volver_home.click()
 
+title = driver.title
+if "INICIO - CENTRO ESTANT" == title:
+    print("Regreso a la página principal: "+Fore.GREEN+"PASS") 
+else:
+    print("Regreso a la página principal: "+Fore.RED+"FAIL")
+    contar_error += 1
+
 time.sleep(2)
+
+if contar_error == 0:
+    print(Back.GREEN+"Test: Envío del formulario vacío en el link 'Contacto': PASS")
+else:
+    print(Back.RED+"Test: Envío del formulario vacío en el link 'Contacto': FAIL")
+
 driver.close()
 
-print("Test: Funcionamiento del link 'Contacto': Pass")
+
--- TC_PlayVideo_Us.py
+++ TC_PlayVideo_Us.py
@@ -3,7 +3,9 @@ from selenium.webdriver.common.by import By
 from pynput.mouse import Button, Controller
 import os
 import time
+from colorama import Fore, init, Back
 
+init(autoreset=True)
 os.system("cls")
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
@@ -11,35 +13,69 @@ options.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver=webdriver.Chrome(executable_path=r"C:\Drivers\chromedriver.exe",options=options)
 driver.maximize_window()
 
+contar_error = 0
+
 #Abrir sitio web
 driver.get("https://centroestant.com.ar/")
 title = driver.title
-assert "INICIO - CENTRO ESTANT" == title, "No coinciden las páginas"
+if "INICIO - CENTRO ESTANT" == title:
+    print("Ingreso a página principal: "+Fore.GREEN+"PASS") 
+else:
+    print("Ingreso a página principal: "+Fore.RED+"FAIL")
+    contar_error += 1
 
 #Abrir link "Nosotros"
 click_button = driver.find_element(by=By.LINK_TEXT, value="Nosotros")
 click_button.click()
 
 nosotros = "Somos una empresa argentina, dedicada a la producción de muebles."
-h3 = driver.find_element(by=By.XPATH,value="//*[@id='text-box-1956396795']/div/div/h3/strong").text
-assert nosotros == h3, "No se ingreso a la seccion 'Nosotros'"
+h3 = driver.find_element(by=By.XPATH,value="/html/body/div[1]/main/div/div[1]/div[1]/div[2]/div[2]/div/div/h3/strong").text
+if nosotros == h3:
+    print("Ingreso a la sección 'Nosotros': "+Fore.GREEN+"PASS") 
+else:
+    print("Ingreso a la sección 'Nosotros': "+Fore.RED+"FAIL")
+    contar_error += 1
 
 time.sleep(4)
 
 #Reproducir video
-mouse=Controller()
-driver.execute_script("window.scrollTo(0,600)")
-mouse.position = (800,700)
-time.sleep(2)
-mouse.press(Button.left)
-mouse.release(Button.left)
-time.sleep(5)
+try:
+    reproducir_video = True
+    mouse=Controller()
+    driver.execute_script("window.scrollTo(0,600)")
+    mouse.position = (800,700)
+    time.sleep(2)
+    mouse.press(Button.left)
+    mouse.release(Button.left)
+    time.sleep(5)
+    print("Reproducción del video: "+Fore.GREEN+"PASS") 
+except:
+    print("Reproducción del video: "+Fore.RED+"FAIL") 
+    reproducir_video = False
+    
+
+#Existencia de imagen
+imag = "https://s.lamason.us/centroestant.com.ar/media/2022/02/18172951/1_CME120P_FondoBlanco_1000x1000-1024x1024.jpg"
+imag2 = driver.find_element(by=By.XPATH,value="/html/body/div[1]/main/div/div[9]/div[2]/div/div[2]/div/img").text
 
 #Volver a la home
-volver_home = driver.find_element(by=By.XPATH, value="//*[@id='logo']/a")
+volver_home = driver.find_element(by=By.XPATH, value="/html/body/div[1]/header/div/div[2]/div[1]/div[1]/a/img[1]")
 volver_home.click()
 
+title = driver.title
+if "INICIO - CENTRO ESTANT" == title:
+    print("Regreso a la página principal: "+Fore.GREEN+"PASS") 
+else:
+    print("Regreso a la página principal: "+Fore.RED+"FAIL")
+    contar_error += 1
+
 time.sleep(2)
+
+if contar_error == 0 and reproducir_video == True:
+    print(Back.GREEN+"Test: Funcionamiento del video en el link 'Nosotros': PASS")
+else:
+    print(Back.RED+"Test: Funcionamiento del video en el link 'Nosotros': FAIL")
+
 driver.close()
 
-print("Test: Funcionamiento del link 'Nosotros': Pass")
+
